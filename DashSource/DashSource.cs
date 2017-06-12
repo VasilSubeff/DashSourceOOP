418a63ac8fcc8f113f08085bbd5c581872704382
Vasil Sabev <vasil.sabev@gmail.com>
2017-06-13 00:12:03 +0300

    first

---
+++ DashSource/DashSource.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashSource
+{
+    class DashSource
+    {
+        static void Main(string[] args)
+        {
+            string sourceFilePath = @"C:\Users\vasil\Desktop\Dimo\KPI_Status_12-Jun-2017.csv";
+            string tableName = "dr_kpis";
+
+            Loader ldr = new Loader();
+            var columnNames = ldr.getTableColumns(tableName);
+            
+            var parsedFile = ldr.GetDataTabletFromCSVFile(sourceFilePath);
+            ldr.InsertDataIntoSQLServerUsingSQLBulkCopy(parsedFile, tableName, columnNames);           
+        }
+        
+    }
+}
---
+++ DashSource/Loader.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DashSource
+{
+    public class Loader
+    {
+        private string connString;
+        private string delimiter;
+
+        public string ConnectionString
+        {
+            get
+            {
+                return Properties.Settings.Default.ConnectionString;
+            }
+        }
+
+        public string Delimiter
+        {
+            get
+            {
+                return Properties.Settings.Default.Delimiter;
+            }
+        }
+
+        public List<string> getTableColumns(string tableName)
+        {
+            List<string> listOfColumns = new List<string>();
+            using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "select c.name from sys.columns c inner join sys.tables t on t.object_id = c.object_id and t.name = '" + tableName + "' and t.type = 'U'";
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            listOfColumns.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+
+            return listOfColumns;
+        }
+
+        public DataTable GetDataTabletFromCSVFile(string sourceFilePath)
+        {
+            DataTable pipeData = new DataTable();
+            try
+            {
+                using (TextFieldParser pipeReader = new TextFieldParser(sourceFilePath))
+                {
+                    pipeReader.SetDelimiters(new string[] { this.Delimiter });
+                    pipeReader.HasFieldsEnclosedInQuotes = true;
+
+                    string[] colFields = pipeReader.ReadFields();
+                    foreach (string column in colFields)
+                    {
+                        DataColumn datecolumn = new DataColumn(column);
+                        datecolumn.AllowDBNull = true;
+                        pipeData.Columns.Add(datecolumn);
+                    }
+                    while (!pipeReader.EndOfData)
+                    {
+                        string[] fieldData = pipeReader.ReadFields();
+                        //Making empty value as null.
+                        for (int i = 0; i < fieldData.Length; i++)
+                        {
+                            if (fieldData[i] == "")
+                            {
+                                fieldData[i] = null;
+                            }
+                        }
+                        pipeData.Rows.Add(fieldData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return pipeData;
+        }
+
+        public void InsertDataIntoSQLServerUsingSQLBulkCopy(DataTable fileData, string tableName, List<string> columnNames)
+        {
+            using (SqlConnection dbConnection = new SqlConnection(this.ConnectionString))
+            {
+                dbConnection.Open();
+                using (SqlBulkCopy s = new SqlBulkCopy(dbConnection))
+                {
+                    s.DestinationTableName = tableName;
+                    var columns = fileData.Columns;
+
+                    for (int i = 0; i < columns.Count; i++)
+                    {
+                        s.ColumnMappings.Add(columns[i].ToString(), columnNames[i].ToString());
+                    }
+                    s.WriteToServer(fileData);
+                }
+            }
+        }
+    }
+}
